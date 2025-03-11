0a0921a095e3ab72254dcd7a56c2e49d0f7b85ba
Anna Beatriz <anna.b.valentim@aluno.senai.br>
2025-03-11 15:47:00 -0300

    update

--- app-navigation/App.js
+++ app-navigation/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import Stack1 from './navigation/Dr';
+import Stack1 from './navigation/DrawerNavigator';
 import Stack2 from '../app-navigation/pages/Stack2';
-//import Stack3 from './pages/StackScreen3';
+import Stack3 from './navigation/TabNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack2 />
+      <Stack3 />
     </NavigationContainer>
   );
 }
---
+++ app-navigation/navigation/TabNavigator.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import TabScreen from "../pages/TabScreen";
+import Tab2 from "../pages/Tab2";
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigator() {
+    return (
+        <Tab.Navigator initialRouteName="Tab Screen">
+                <Tab.Screen 
+                    name="Tab Screen" 
+                    component={TabScreen} 
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <Icon name="home" color={color} size={size} />
+                        ),
+                    }}
+                />
+            <Tab.Screen name="Tab2" component={Tab2} />
+        </Tab.Navigator>
+    );
+}
